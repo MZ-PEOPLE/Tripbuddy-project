c080b53bfc9959079157e4105029c014855d3598
77JY <kwn03111@naver.com>
2023-11-28 16:44:09 +0900

    feat: 로그인 상태 조건부 렌더링 구현

--- components/topbar/LoginCheck.js
+++ components/topbar/LoginCheck.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import AlertBell from "./AlertBell";
+import LoginBtn from "./LoginButton";
 
 const LoginCheck = () => {
-  const [login, setLogin] = useState(false);
+  const [login, setLogin] = useState(true);
 
-  return login ? <div>알람창</div> : <div>로그인하셈</div>;
+  return login ? <AlertBell /> : <LoginBtn />;
 };
 
 export default LoginCheck;
