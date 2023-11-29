ba0b8c11bafc8bcfe39edd51bf942bd03fda41e8
77JY <kwn03111@naver.com>
2023-11-29 17:37:36 +0900

    feat: topbar에 searchbar 컴포넌트 조건형 추가
    topbar를 불러올때 searchBar = true일때 불러오도록 구성

--- components/topbar/TopBar.js
+++ components/topbar/TopBar.js
@@ -1,14 +1,19 @@
 import styles from "./TopBar.module.css";
+import SearchBar from "./SearchBar";
 
-const Topbar = ({ leftContent, middleContent, rightContent, next }) => {
+const Topbar = ({ leftContent, middleContent, rightContent, searchBar = false }) => {
   return (
     <nav>
-      <div className={styles.Header}>
-        <div className={styles.left}>{leftContent}</div>
-        <div className={styles.middle}>{middleContent}</div>
-        <div className={styles.right}>{rightContent}</div>
+      <div className={styles.container}>
+        <div className={`${styles.Header} ${searchBar ? styles.Header_searchTrue : null}`}>
+          <div className={styles.Header_bar}>
+            <div className={styles.left}>{leftContent}</div>
+            <div className={styles.middle}>{middleContent}</div>
+            <div className={styles.right}>{rightContent}</div>
+          </div>
+          {searchBar ? <SearchBar /> : null}
+        </div>
       </div>
-      {next}
     </nav>
   );
 };
