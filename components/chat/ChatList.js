1dc4aa335204092cc16e3ac62755839e67a674d1
yeong <gukgu6060@gmail.com>
2023-11-30 15:52:55 +0900

    Design: 채팅방 목록 기본 틀 작업(진행중)

--- components/chat/ChatList.js
+++ components/chat/ChatList.js
@@ -3,10 +3,19 @@ import styles from "./ChatList.module.css";
 
 export default function ChatList() {
     return (
-        <div className={styles.frame}>
-            <div>
-                <img className={styles.profileImg} src="/img/chatProfile" />
-
+        <div>
+            <div className={styles.userFrame}>
+                <div className={styles.imgFrame}>
+                    <img className={styles.profileImg} src="/img/chatProfile.png" />
+                </div>
+                <div className={styles.chatContextFrame}>
+                    <div className={styles.userIdFrame}>
+                        <div className={styles.profileId}>치와와와</div>
+                    </div>
+                    <div className={styles.userChatFrame}>
+                        <div className={styles.chatContext}>반가워요 와와에요오~</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
