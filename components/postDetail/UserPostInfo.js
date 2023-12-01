6fa074efd2084e16ed6f6d4939ba6e89fbbc38a5
77JY <kwn03111@naver.com>
2023-12-01 21:46:35 +0900

    feat: 게시글의 컴포넌트 userpostinfo 생성
    userPostInfo의 컴포넌트는 총 7가지의 정보를 받아옴
    여행 모집인원, 좋아요수, 여행위치, 타이틀,
    유저네임, 유저프로필url, 여행시작날~종료날
    
    추후 컴포넌트를 4가지로 분리후 합칠예정

---
+++ components/postDetail/UserPostInfo.js
@@ -0,0 +1,43 @@
+import styles from "./UserPostInfo.module.css";
+import { FaUserGroup } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+import { CiLocationOn } from "react-icons/ci";
+
+/* 추후 컴포넌트 분리작업 이 필요한 것 같음
+비대하진 않으나 무슨작업을 하는지 잘 모르겠음
+4가지 컴포넌트로 분해후 불러오면 읽기 쉬울듯
+*/
+const UserPost = ({ items }) => (
+  <div className={styles.user_post}>
+    <div className={styles.post_info}>
+      <div className={styles.post_box}>
+        <div className={styles.post_iconBox}>
+          <FaUserGroup className={styles.post_iconSize} />
+          <span className={styles.post_info_fontSize}>{items.numberOfPeople}</span>
+        </div>
+        <div className={styles.post_iconBox}>
+          <FaRegHeart className={`${styles.post_iconSize} ${styles.post_heart_icon}`} />
+          {/* 하트를 이모티콘이 아닌 버튼으로 교체작업 필요 + 클릭 true일때 빨간하트, false일때 회색
+          백엔드 작업이 되었을땐 유저가 좋아요를 눌렀는지 호출 필요 */}
+          <span className={styles.post_info_fontSize}>{items.like}</span>
+        </div>
+      </div>
+      <div className={styles.post_location}>
+        <CiLocationOn className={styles.post_iconSize} />
+        <span className={styles.post_info_fontSize}>{items.location}</span>
+      </div>
+      <div className={styles.post_title}>{items.title}</div>
+      <div className={styles.userInfo}>
+        <div className={styles.userImgBox}>
+          <img src={items.userImg} className={styles.userImg} />
+        </div>
+        <div className={styles.userInfoDetail}>
+          <div className={`${styles.userName} ${styles.userInfoDetailBox}`}>{items.username}</div>
+          <div className={`${styles.timeStemp} ${styles.userInfoDetailBox}`}>{items.timeStemp}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default UserPost;
