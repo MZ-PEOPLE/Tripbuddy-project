c38aaea33538444f12b7364d2706b344f21b4d38
dappyiness <dappyiness@gmail.com>
2023-12-05 10:23:21 +0900

    refactor: 프로필 class명 수정

--- components/MyPage/UserProfileDetail.js
+++ components/MyPage/UserProfileDetail.js
@@ -3,8 +3,8 @@ import styles from "./UserProfileDetail.module.css";
 function UserProfileDetail() {
   return (
     <div>
-      <div className={styles.userProfileTopMainContainer}>
-        <div className={styles.userProfileTopMain}>
+      <div className={styles.userProfileContainer}>
+        <div className={styles.userProfileBox}>
           <div className={styles.userProfile}>
             <div className={styles.profilePicContainer}>
               <img
@@ -12,28 +12,28 @@ function UserProfileDetail() {
                 src="https://trboard.game.onstove.com/Data/TR/20170728/19/636368659390474572.jpg"
               />
             </div>
-            <div className={styles.profileInforDetail}>
-              <div className={styles.profileName}>치와와와와 님</div>
-              <div className={styles.profileAgeGender}>
-                <div className={styles.profileAge}>20대</div>
-                <div className={styles.profileGender}>중성화</div>
+            <div className={styles.profileDetail}>
+              <div className={styles.Name}>치와와와와 님</div>
+              <div className={styles.ageGenderBox}>
+                <div className={styles.age}>20대</div>
+                <div className={styles.gender}>중성화</div>
               </div>
             </div>
           </div>
-          <div className={styles.profileDescrip}>
+          <div className={styles.Descrip}>
             왈왈왈! 왈!왈왈왈! 왈!왈왈왈! 왈!왈왈왈!
           </div>
         </div>
       </div>
 
       <div className={styles.profileModify}>
-        <button type="button" className={styles.profileModifyButton}>
+        <button type="button" className={styles.ModifyButton}>
           <img
             src="https://cdn-icons-png.flaticon.com/128/747/747376.png"
             alt="아이콘 자리"
-            className={styles.profileModifyIcon}
+            className={styles.ModifyIcon}
           />
-          <div className={styles.profileModifyText}>프로필 수정</div>
+          <div className={styles.ModifyText}>프로필 수정</div>
         </button>
       </div>
     </div>
