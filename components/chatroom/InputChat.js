b63f5e62760548f86e373c905dbd373fb945c3b1
yeong <gukgu6060@gmail.com>
2023-12-06 11:58:49 +0900

    feat:채팅창의 input박스 구현

---
+++ components/chatroom/InputChat.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./InputChat.module.css";
+
+export default function InputChat() {
+    return (
+        <input className={styles.inputBar}></input>
+    );
+
+}
