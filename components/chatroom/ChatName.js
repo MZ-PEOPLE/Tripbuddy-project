04c232e86fb29985dac8b745cd76650a45544cd2
yeong <gukgu6060@gmail.com>
2023-12-06 12:06:46 +0900

    styles:탑바의 middleContent를 그룹채팅방으로 변경

---
+++ components/chatroom/ChatName.js
@@ -0,0 +1,12 @@
+import styles from "./ChatBar.module.css";
+import { IoMenu } from "react-icons/io5";
+
+
+export default function ChatBar() {
+    return (
+        <chatname>
+            <div>그룹 채팅방</div>
+
+        </chatname>
+    )
+}
