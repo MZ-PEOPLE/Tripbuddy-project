c78fdac1628e9fac9d369f311596521b5d566e6f
yeong <gukgu6060@gmail.com>
2023-12-06 12:26:02 +0900

    feat:채팅방 메뉴 열고 닫는 기능 구현

---
+++ components/chatroom/menu/MenuDetail.js
@@ -0,0 +1,16 @@
+import ChatInfo from "./ChatInfo"
+import style from "./MenuDetail.module.css"
+
+function MenuDetail({ bar, setBar }) {
+    return (
+        bar && (
+            <div className={style.frame}> {/* {styles.openMenu} */}
+                <div>
+                    <ChatInfo className={style.exitMenu} />
+                </div>
+            </div>
+        )
+    )
+}
+
+export default MenuDetail
