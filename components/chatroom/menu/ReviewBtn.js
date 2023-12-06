2453f9fc122992cf2b0d3cc07b87baf33732e43a
yeong <gukgu6060@gmail.com>
2023-12-06 12:23:12 +0900

    feat:채팅방 메뉴의 동행후기작성 버튼 생성

---
+++ components/chatroom/menu/ReviewBtn.js
@@ -0,0 +1,10 @@
+import styles from "./ReviewBtn.module.css";
+
+export default function ReviewBtn() {
+    return (
+        <div className={styles.frame}>
+            <div className={styles.reviewbtn}>동행후기 작성<button></button></div>
+        </div>
+
+    )
+}
