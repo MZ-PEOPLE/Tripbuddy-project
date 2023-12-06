56b5b2097339b7582ad77063b0ddf74c088b99f1
77JY <kwn03111@naver.com>
2023-12-06 16:20:43 +0900

    chore: 카카오 로그인기능의 주석만 추가

--- pages/api/auth/kakao.js
+++ pages/api/auth/kakao.js
@@ -11,14 +11,17 @@ export default async function handler(req, res) {
       let user = await User.findOne({ id });
 
       if (!user) {
+        // db에 유저 없을경우 생성
+        console.log("새로운 유저 업데이트");
         user = new User({ id, name: nickname, profileImage: profile_image, age });
         await user.save();
       }
-
+      // 확인 후 유저 토큰 발행
       const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
       console.log(user, token);
-      res.status(200).json({ message: "로그인 성공", user, token });
+      // 토큰 발행 후 토큰 전송
+      res.status(200).json({ message: "로그인 성공", token });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
