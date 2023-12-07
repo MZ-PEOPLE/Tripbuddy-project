303c4f268e98b38022f83e965049520470f159a9
yeong <gukgu6060@gmail.com>
2023-12-07 17:56:39 +0900

    chore:아이디 수정 컴포넌트 분리

---
+++ components/mypageUpdate/IdUpdate.js
@@ -0,0 +1,31 @@
+import styles from "./IdUpdate.module.css";
+import React, { useState, useRef } from 'react';
+import { RxPencil1 } from "react-icons/rx";
+
+export default function IdUpdate() {
+
+    const idInputRef = useRef('');
+    const [newId, setNewId] = useState('');
+
+    const handleIdChange = (event) => {
+        setNewId(event.target.value);
+    };
+    const idUpdateClick = () => {
+        console.log("아이디 수정", newId);
+    };
+
+    return (
+        <div className={styles.idFrame}>
+            <div className={styles.profileId}>아이디</div>
+            <input
+                type="text"
+                className={styles.idUpdate}
+                value={newId}
+                onChange={handleIdChange}
+                ref={idInputRef}
+            />
+            <button className={styles.idButton} onClick={idUpdateClick}><RxPencil1 /></button>
+
+        </div>
+    )
+}
