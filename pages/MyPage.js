c5401569440fe81f7757c42646596af525d984fd
dappyiness <dappyiness@gmail.com>
2023-12-08 14:43:25 +0900

    feat: 마이페이지, 리뷰 작성 뷰페이지 생성

---
+++ pages/MyPage.js
@@ -0,0 +1,14 @@
+import MyPageProfileDetail from "@/components/MyPage/UserProfileDetail";
+import MyPageUserTapBar from "@/components/MyPage/UserTapBar";
+import MyPagePostContents from "@/components/MyPage/UserPostContents";
+import MyPageTogetherContents from "@/components/MyPage/UserTogetherContents";
+import MyPageUserReview from "@/components/MyPage/UserReview";
+
+export default function MyPageContainer() {
+  return (
+    <>
+      <MyPageProfileDetail />
+      <MyPageUserTapBar />
+    </>
+  );
+}
