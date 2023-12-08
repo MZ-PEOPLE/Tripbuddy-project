1de5ac8a72438074b561af4144b0cea4ed04aae5
77JY <kwn03111@naver.com>
2023-12-08 10:36:08 +0900

    feat: 메인페이지 유저 포스트 추가 + Navbar -> Topbar 이름변경

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
-import NavBar from "@/components/topbar/TopBar";
 import BackBtn from "@/components/topbar/BackButton";
 import LoginCheck from "@/components/topbar/LoginCheck";
 import Logo from "@/components/topbar/Logo";
 import FooterBar from "@/components/footerbar/FooterBar";
+import UserPoster from "@/components/home/UserPoster";
+import Topbar from "@/components/topbar/TopBar";
 
 export default function Home() {
   return (
     <>
-      <NavBar
+      <Topbar
         leftContent={<BackBtn />}
         middleContent={<Logo />}
         rightContent={<LoginCheck />}
+        searchBar={true}
       />
+      <UserPoster />
       <FooterBar />
     </>
   );
