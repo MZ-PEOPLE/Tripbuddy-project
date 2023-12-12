6bad4ee958b1f65bb0608f63bb86358c565ed174
77JY <kwn03111@naver.com>
2023-12-12 19:04:32 +0900

    fix: 몽구스 스키마 정의 구문 오류 수정
    몽구스 스키마 정의의 잘못된 괄호를 수정함

--- pages/api/lib/models/write.model.js
+++ pages/api/lib/models/write.model.js
@@ -7,20 +7,22 @@ const writeSchema = new mongoose.Schema(
     image: [String],
     dateSelectData: {
       startDate: Date,
-      endDate: Date
+      endDate: Date,
     },
     genderAgeSelectData: {
       gender: String,
-      ageRange: String
+      ageRange: String,
     },
     headSelectData: {
-      headCounts: String
+      headCounts: String,
     },
-    travelMapData: { /* 여행지도 구조 정의 */ },
+    travelMapData: {
+      /* 여행지도 구조 정의 */
+    },
+  },
   {
     timestamps: true,
   }
-}
 );
 
 export default mongoose.models.Write || mongoose.model("Write", writeSchema);
