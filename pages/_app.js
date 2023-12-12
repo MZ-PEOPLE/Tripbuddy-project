aec82a5c99af0f79ea66eac682cf6f38e5894924
77JY <kwn03111@naver.com>
2023-12-12 18:57:35 +0900

    feat: 사용자 검증 최적화 및 로딩 상태 관리 추가
    url경로가 바뀌었을때만 유저정보 새롭게 호출하도록 변경

--- pages/_app.js
+++ pages/_app.js
@@ -3,45 +3,54 @@ import axios from "axios";
 import "../styles/global.css";
 import { useRouter } from "next/router";
 
-const verifyUser = async (setUser) => {
+const verifyUser = async (setUser, setLoading) => {
   try {
+    setLoading(true); // 로딩 시작
     const token = localStorage.getItem("token");
-    console.log(token);
     if (token) {
-      console.log("토큰은있음");
       const response = await axios.get("/api/logined", {
         headers: { Authorization: `Bearer ${token}` },
       });
       if (response.status === 200) {
         setUser(response.data);
-        console.log(response.data.profileImage);
       }
     }
   } catch (error) {
-    console.log("실패");
     console.error("인증 실패:", error);
     localStorage.removeItem("token");
+  } finally {
+    setLoading(false); // 로딩 끝
   }
 };
 
 function MyApp({ Component, pageProps }) {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   useEffect(() => {
-    verifyUser(setUser);
+    let lastPath = router.asPath; // 마지막 경로 저장
 
-    const handleRouteChange = () => {
-      verifyUser(setUser);
+    const handleRouteChange = (url) => {
+      if (lastPath !== url) {
+        verifyUser(setUser, setLoading);
+        lastPath = url; // 마지막 url경로 업데이트
+      }
     };
 
     router.events.on("routeChangeComplete", handleRouteChange);
 
+    // 첫 로딩 시 검증
+    verifyUser(setUser, setLoading);
+
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
-  }, [router.events]);
-  /* 사실 페이지를 이동할때마다 유저정보를 조회하는게 아닌 로그인했을때 + 유저정보를 바꿨을때만 바꿔서 보여주긴 해야하는데 혹시 모바일로 접속해 바꾸고 컴퓨터로 계속 보고있으면 바뀌지않음 일딴은 이렇게 페이지가 바뀔때마다 조회하도록 하겠음 추후 생각 하도록 */
+  }, []);
+
+  if (loading) {
+    return null; // 로딩일때
+  }
   return <Component {...pageProps} user={user} />;
 }
 
