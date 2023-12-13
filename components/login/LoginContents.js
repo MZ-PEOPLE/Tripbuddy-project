6f74aa72d75a96000db1836a4c9975321d41fdec
dappyiness <dappyiness@gmail.com>
2023-12-13 17:04:56 +0900

    design: 로그인 화면 이미지 추가 및 CSS 수정

--- components/login/LoginContents.js
+++ components/login/LoginContents.js
@@ -1,15 +1,21 @@
 import styles from "./LoginContents.module.css";
 import Logo from "../topbar/Logo";
+import { ImAirplane } from "react-icons/im";
+
+const ImgSrc = "/img/loginimg6.png";
 
 function LoginContents() {
   return (
     <div className={styles.container}>
       <div className={styles.imageBox}>
         <Logo />
+        <ImAirplane className={styles.icon} />
       </div>
-      <div className={styles.text}>
-        로그인하고<br></br> 여행을 계속해요
+      <div className={styles.loginImage}>
+        <img className={styles.img} src={ImgSrc} alt="Profile" />
       </div>
+
+      <div className={styles.text}>로그인하고 여행을 계속해요</div>
     </div>
   );
 }
