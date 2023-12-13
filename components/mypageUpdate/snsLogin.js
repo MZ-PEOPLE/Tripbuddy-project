404f803979d612dddb36c1d3ac09b6ddfcde3b3a
yeong <gukgu6060@gmail.com>
2023-12-13 22:51:36 +0900

    design:sns연계 아이콘 수정

--- components/mypageUpdate/snsLogin.js
+++ components/mypageUpdate/snsLogin.js
@@ -1,16 +1,21 @@
-import { FaSquareInstagram } from "react-icons/fa6";
-import { FaFacebookSquare } from "react-icons/fa";
+import { HStack, Button } from "@chakra-ui/react";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 import styles from "./snsLogin.module.css";
 
-export default function snsLogin() {
+export default function SnsLogin() {
     return (
         <div className={styles.loginFrame}>
             <div className={styles.profileLogin}>소설 계정 연동</div>
-            <div className={styles.snsLoginFrame}>
-                <FaFacebookSquare className={styles.loginFacebook} />
-                <FaSquareInstagram className={styles.loginInstagram} />
-            </div>
+            <HStack className={styles.snsLoginFrame}>
+                <Button colorScheme="facebook" leftIcon={<FaFacebook />} className={styles.loginFacebook}>
+                    Facebook
+                </Button>
+            </HStack>
+            <HStack className={styles.snsLoginFrame}>
+                <Button colorScheme="teal" leftIcon={<FaInstagram />} className={styles.loginInstagram}>
+                    Instagram
+                </Button>
+            </HStack>
         </div>
-    )
+    );
 }
-
