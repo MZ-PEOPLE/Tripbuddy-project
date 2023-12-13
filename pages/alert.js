459af1623ddaa3f294e948e8ccf4791f15a97a07
77JY <kwn03111@naver.com>
2023-12-13 09:17:39 +0900

    refactor: 헤더와 푸터를 pages에서 로드하도록 변경

--- pages/alert.js
+++ pages/alert.js
@@ -1,5 +1,14 @@
+import FooterBar from "@/components/footerbar/FooterBar";
 import Notifications from "@/components/notification/Notifications";
+import BackBtn from "@/components/topbar/BackButton";
+import Topbar from "@/components/topbar/TopBar";
 
 export default function Alarm({ user }) {
-  return <Notifications />;
+  return (
+    <>
+      <Topbar leftContent={<BackBtn />} middleContent={"알림"} />
+      <Notifications />
+      <FooterBar profileImage={user ? user.profileImage : null} />
+    </>
+  );
 }
