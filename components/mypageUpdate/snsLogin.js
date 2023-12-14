7e94f63f7ecbde62fd2c598717db505ba0cc5b49
yeong <gukgu6060@gmail.com>
2023-12-14 17:33:41 +0900

    style:세미콜론, 안쓰는 클래스네임 삭제

--- components/mypageUpdate/snsLogin.js
+++ components/mypageUpdate/snsLogin.js
@@ -6,12 +6,12 @@ export default function SnsLogin() {
     return (
         <div className={styles.loginFrame}>
             <div className={styles.snsLogin}>소설 계정 연동</div>
-            <HStack className={styles.snsLoginFrame}>
+            <HStack>
                 <Button colorScheme="facebook" leftIcon={<FaFacebook />} className={styles.loginFacebook}>
                     Facebook
                 </Button>
             </HStack>
-            <HStack className={styles.snsLoginFrame}>
+            <HStack>
                 <Button colorScheme="teal" leftIcon={<FaInstagram />} className={styles.loginInstagram}>
                     Instagram
                 </Button>
