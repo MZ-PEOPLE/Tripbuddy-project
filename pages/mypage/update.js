ccb7e62c9bf460102ecace5d37976ef9252d2afb
yeong <gukgu6060@gmail.com>
2023-12-14 16:08:09 +0900

    feat:마이페이지 수정 탑바, 푸터바 추가

--- pages/mypage/update.js
+++ pages/mypage/update.js
@@ -1,7 +1,26 @@
 import ProfileUpdate from "@/components/mypageUpdate/ProfileUpdate";
+import BackBtn from "@/components/topbar/BackButton";
+import LoginCheck from "@/components/topbar/LoginCheck";
+import Topbar from "@/components/topbar/TopBar";
+import FooterBar from "@/components/footerbar/FooterBar";
+import { useRouter } from "next/router";
 
-export default function MypageUpdate() {
-    return (
-        <ProfileUpdate />
-    )
+export default function MypageUpdate({ user }) {
+  const router = useRouter();
+  if (!user) {
+    // 사용자 정보가 없으면 렌더링 x , 로그인 페이지로 이동
+    router.push("/login");
+    return null;
+  }
+  return (
+    <>
+      <Topbar
+        leftContent={<BackBtn />}
+        middleContent={<>마이페이지 수정</>}
+        rightContent={<LoginCheck isLogin={user ? true : false} />}
+      />
+      <ProfileUpdate />
+      <FooterBar />
+    </>
+  )
 }
