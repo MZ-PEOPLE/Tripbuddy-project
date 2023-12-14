f7cd49f04c32818c2a68cde3d0e4890446e47558
yeong <gukgu6060@gmail.com>
2023-12-14 16:02:47 +0900

    feat:chat페이지에 푸터바, 탑바 추가

--- pages/chat.js
+++ pages/chat.js
@@ -1,4 +1,19 @@
 import ChatListComponent from "@/components/chat/ChatList";
+import BackBtn from "@/components/topbar/BackButton";
+import LoginCheck from "@/components/topbar/LoginCheck";
+import Topbar from "@/components/topbar/TopBar";
+import FooterBar from "@/components/footerbar/FooterBar";
 export default function ChatList({ user }) {
-  return <ChatListComponent />;
+  return (
+    <>
+      <Topbar
+        leftContent={<BackBtn />}
+        middleContent={"채팅"}
+        rightContent={<LoginCheck isLogin={user ? true : false} />}
+      />
+      <ChatListComponent />
+      <FooterBar />
+    </>
+  )
+
 }
