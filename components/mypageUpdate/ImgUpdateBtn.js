f0be6f26e0dd746b01142f243667247430d88954
77JY <kwn03111@naver.com>
2023-12-15 11:49:27 +0900

    feat: ImgUpdateBtn 컴포넌트에 profileImage prop 추가
    초기 이미지로 profileImage prop 사용
    사용자가 이미지를 업데이트할 수 있도록 로직 구현

--- components/mypageUpdate/ImgUpdateBtn.js
+++ components/mypageUpdate/ImgUpdateBtn.js
@@ -1,54 +1,44 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 import { PiCameraDuotone } from "react-icons/pi";
 import { FaTrashAlt } from "react-icons/fa";
 import styles from "./ImgUpdateBtn.module.css";
 
+export default function ImgUpdateBtn({ profileImage }) {
+  const fileInputRef = useRef(null);
+  const [selectedImage, setSelectedImage] = useState(profileImage);
 
-export default function ImgUpdateBtn() {
-    const fileInputRef = useRef(null);
-    const [selectedImage, setSelectedImage] = useState(null);
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
 
-    const handleButtonClick = () => {
-        fileInputRef.current.click();
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const imageDataUrl = reader.result;
+      setSelectedImage(imageDataUrl);
     };
+    reader.readAsDataURL(selectedFile);
+  };
 
-    const handleFileChange = (event) => {
-        const selectedFile = event.target.files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-            const imageDataUrl = reader.result;
-            setSelectedImage(imageDataUrl);
-        };
-        reader.readAsDataURL(selectedFile);
-    };
+  const handleDeleteButtonClick = () => {
+    setSelectedImage(null);
+  };
 
-    const handleDeleteButtonClick = () => {
-        setSelectedImage(null);
-    };
-
-    return (
-        <div className={styles.mainFrame}>
-            <div>
-                <div className={styles.frame}>
-                    <button onClick={handleButtonClick} >
-                        <PiCameraDuotone className={styles.imgUpdateBtn} />
-                    </button>
-                    <img className={styles.profileImg} src={selectedImage} />
-                </div>
-            </div>
-            <input
-                type="file"
-                accept="image/*"
-                style={{ display: 'none' }}
-                ref={fileInputRef}
-                onChange={handleFileChange}
-            />
-            {selectedImage && (
-                <button className={styles.deleteBtn} onClick={handleDeleteButtonClick}><FaTrashAlt /></button>
-            )}
-        </div>
-
-    );
+  return (
+    <div className={styles.mainFrame}>
+      <div className={styles.frame}>
+        <button onClick={handleButtonClick}>
+          <PiCameraDuotone className={styles.imgUpdateBtn} />
+        </button>
+        <img className={styles.profileImg} src={selectedImage} />
+      </div>
+      <input type="file" accept="image/*" style={{ display: "none" }} ref={fileInputRef} onChange={handleFileChange} />
+      {selectedImage && (
+        <button className={styles.deleteBtn} onClick={handleDeleteButtonClick}>
+          <FaTrashAlt />
+        </button>
+      )}
+    </div>
+  );
 }
-
-
