1291ebebd09567587f08e4061f16d7524305db69
dappyiness <dappyiness@gmail.com>
2023-12-15 17:17:51 +0900

    design: 홈화면 CSS 수정

--- components/home/PosterDetail.js
+++ components/home/PosterDetail.js
@@ -1,15 +1,25 @@
 import styles from "./PosterDetail.module.css";
+import { BiCalendar } from "react-icons/bi";
+import { BiMaleFemale } from "react-icons/bi";
+import { BiAlarm } from "react-icons/bi";
 
 const PosterDetail = ({ day, schedule, userInfo }) => {
   // user에는 20대 여성 days에는 여행 일 수 , 시작 날짜, 끝날 날짜
 
   return (
     <div className={styles.infoBox}>
-      <div className={styles.items}>{day}일</div>
       <div className={styles.items}>
+        <BiAlarm className={styles.icon} />
+        {day}일
+      </div>
+      <div className={styles.items}>
+        <BiCalendar className={styles.icon} />
         {schedule.start} ~ {schedule.end}
       </div>
-      <div className={styles.items}>{userInfo}</div>
+      <div className={styles.items}>
+        <BiMaleFemale className={styles.icon} />
+        {userInfo}
+      </div>
     </div>
   );
 };
