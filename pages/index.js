96d5d3d8c981ed7b37c6a7e4f815f0ea4738a6f0
dappyiness <dappyiness@gmail.com>
2023-12-15 21:29:12 +0900

    design: 홈화면 간격 조정

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,21 @@ import Logo from "@/components/topbar/Logo";
 import FooterBar from "@/components/footerbar/FooterBar";
 import UserPoster from "@/components/home/UserPoster";
 import Topbar from "@/components/topbar/TopBar";
+import styles from "@/components/home/index.module.css";
 
 export default function Home({ user }) {
   return (
     <>
-      <Topbar
-        leftContent={<BackBtn />}
-        middleContent={<Logo />}
-        rightContent={<LoginCheck isLogin={user ? true : false} />}
-        searchBar={true}
-      />
-      <UserPoster />
-      <FooterBar profileImage={user ? user.profileImage : null} />
+      <div className={styles.container}>
+        <Topbar
+          leftContent={<BackBtn />}
+          middleContent={<Logo />}
+          rightContent={<LoginCheck isLogin={user ? true : false} />}
+          searchBar={true}
+        />
+        <UserPoster />
+        <FooterBar profileImage={user ? user.profileImage : null} />
+      </div>
     </>
   );
 }
