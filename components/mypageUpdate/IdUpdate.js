1a09f6b1df087b7afadc461df1a352b91c930a83
77JY <kwn03111@naver.com>
2023-12-18 09:28:12 +0900

    refactor: 수정 버튼 삭제 및 상태관리 상향식으로 전달

--- components/mypageUpdate/IdUpdate.js
+++ components/mypageUpdate/IdUpdate.js
@@ -1,16 +1,14 @@
 import styles from "./IdUpdate.module.css";
 import React, { useState, useRef } from "react";
-import { PiPencilSimpleFill } from "react-icons/pi";
 
-export default function IdUpdate({ userName }) {
+export default function IdUpdate({ userName, onUpdate }) {
   const idInputRef = useRef("");
   const [newId, setNewId] = useState(userName);
 
   const handleIdChange = (event) => {
-    setNewId(event.target.value);
-  };
-  const idUpdateClick = () => {
-    console.log("아이디 수정", newId);
+    const updatedId = event.target.value;
+    setNewId(updatedId);
+    onUpdate(updatedId);
   };
 
   return (
@@ -18,9 +16,6 @@ export default function IdUpdate({ userName }) {
       <div className={styles.profileId}>아이디</div>
       <div className={styles.profileName}>
         <input type="text" className={styles.idUpdate} value={newId} onChange={handleIdChange} ref={idInputRef} />
-        <button onClick={idUpdateClick}>
-          <PiPencilSimpleFill className={styles.idButton} />
-        </button>
       </div>
     </div>
   );
