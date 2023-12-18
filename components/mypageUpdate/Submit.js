fdfd46b59db08c047f0fbaa0e8fd16930ab0d9da
77JY <kwn03111@naver.com>
2023-12-18 19:11:51 +0900

    feat: 이미지 업데이트 로직 추가

--- components/mypageUpdate/Submit.js
+++ components/mypageUpdate/Submit.js
@@ -1,14 +1,34 @@
 import styles from "./Submit.module.css";
 import { useRouter } from "next/router";
 
-export default function Submit({ newId, newIntro }) {
+export default function Submit({ newId, newIntro, newImage }) {
   const router = useRouter();
+  const token = localStorage.getItem("token");
 
-  const handleSubmit = async () => {
-    console.log("제출하는 버튼", newId, newIntro);
-    const token = localStorage.getItem("token");
-    console.log(token);
+  const uploadImage = async (image) => {
+    console.log("업로드이미지");
+    const formData = new FormData();
+    formData.append("image", image);
 
+    try {
+      const response = await fetch("/api/image-upload", {
+        headers: { Authorization: `Bearer ${token}` },
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        console.log("업로드성공함", result);
+      } else {
+        throw new Error("업로드실패함");
+      }
+    } catch (error) {
+      console.error("에러임", error);
+    }
+  };
+
+  const updateNameIntro = async (name, about) => {
     const res = await fetch("/api/userUpdate", {
       method: "POST",
       headers: {
@@ -16,8 +36,8 @@ export default function Submit({ newId, newIntro }) {
         Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify({
-        name: newId,
-        about: newIntro,
+        name,
+        about,
       }),
     });
 
@@ -28,6 +48,17 @@ export default function Submit({ newId, newIntro }) {
     } else {
       // 실패했을때 처리 할 로직
       console.error("로그인 실패:", data.error);
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (newImage) {
+      // 새로운 이미지가 존재할때만 업데이트
+      uploadImage(newImage);
+    }
+    if (newId) {
+      // 빈정보 제출 불가능, 이미지는 없어도 되지만 이름은 없으면 안되므로 페이지 변경을 updateNameIntro함수에서 진행함
+      updateNameIntro(newId, newIntro);
     }
   };
   return (
