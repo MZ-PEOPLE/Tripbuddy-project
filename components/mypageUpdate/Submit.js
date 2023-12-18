22e443d9cdec182feec98eb606856c1b72e68756
77JY <kwn03111@naver.com>
2023-12-18 15:26:33 +0900

    feat: 유저 업데이트 기능 구현
    유저 intro와 닉네임 변경기능 구현
    이미지는 추후예정

--- components/mypageUpdate/Submit.js
+++ components/mypageUpdate/Submit.js
@@ -1,8 +1,34 @@
 import styles from "./Submit.module.css";
+import { useRouter } from "next/router";
 
 export default function Submit({ newId, newIntro }) {
+  const router = useRouter();
+
   const handleSubmit = async () => {
     console.log("제출하는 버튼", newId, newIntro);
+    const token = localStorage.getItem("token");
+    console.log(token);
+
+    const res = await fetch("/api/userUpdate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: newId,
+        about: newIntro,
+      }),
+    });
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+      router.push("/mypage");
+    } else {
+      // 실패했을때 처리 할 로직
+      console.error("로그인 실패:", data.error);
+    }
   };
   return (
     <>
