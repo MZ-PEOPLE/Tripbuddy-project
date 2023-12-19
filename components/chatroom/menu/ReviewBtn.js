e9ec3dcd1207b2b6713f35f3382cec0a6614bff0
yeong <gukgu6060@gmail.com>
2023-12-19 18:03:26 +0900

    feat:채팅방 목록의 동행후기 작성버튼과 여행리뷰작성 페이지 연결

--- components/chatroom/menu/ReviewBtn.js
+++ components/chatroom/menu/ReviewBtn.js
@@ -1,10 +1,13 @@
 import styles from "./ReviewBtn.module.css";
+import Link from "next/link";
 
 export default function ReviewBtn() {
     return (
-        <div className={styles.frame}>
-            <div className={styles.reviewbtn}>동행 후기 작성<button></button></div>
-        </div>
+        <Link href={`/reviewwrite`} className={styles.link}>
+            <div className={styles.frame}>
+                <div className={styles.reviewbtn}>동행 후기 작성<button></button></div>
+            </div >
+        </Link>
 
     )
 }
