dcaca3f58da998eef106dbe9ff010a717896ec32
77JY <kwn03111@naver.com>
2023-12-19 09:37:09 +0900

    refactor: 전달받은 프롭스를 형식에 맞춰 가공 후 표현
    페이지에 들어가야할 부분의 프롭스를 양식에 맞춰 가공한 후 담음

--- components/home/PosterDetail.js
+++ components/home/PosterDetail.js
@@ -5,20 +5,30 @@ import { BiAlarm } from "react-icons/bi";
 
 const PosterDetail = ({ day, schedule, userInfo }) => {
   // user에는 20대 여성 days에는 여행 일 수 , 시작 날짜, 끝날 날짜
+  const startDate = new Date(schedule.startDate);
+  const startMonth = startDate.getMonth() + 1;
+  const startDay = startDate.getDate();
+
+  const endDate = new Date(schedule.endDate);
+  const endMonth = endDate.getMonth() + 1;
+  const endDay = endDate.getDate();
+
+  const milliseconds = endDate - startDate;
+  const travleDay = Math.round(milliseconds / (24 * 60 * 60 * 1000)) + 1; // 당일치기 일수도 있으니 + 1일 , 밀리세컨드 단위를 날짜로 변환
 
   return (
     <div className={styles.infoBox}>
       <div className={styles.items}>
         <BiAlarm className={styles.icon} />
-        {day}일
+        {travleDay}일
       </div>
       <div className={styles.items}>
         <BiCalendar className={styles.icon} />
-        {schedule.start} ~ {schedule.end}
+        {startMonth + "/" + startDay}~ {endMonth + "/" + endDay}
       </div>
       <div className={styles.items}>
         <BiMaleFemale className={styles.icon} />
-        {userInfo}
+        {userInfo.ageRange + " " + userInfo.gender}
       </div>
     </div>
   );
