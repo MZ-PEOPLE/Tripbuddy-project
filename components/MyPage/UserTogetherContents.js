d0ef9da81829c32af8b715182cde7d03b5473404
77JY <kwn03111@naver.com>
2023-12-19 20:56:54 +0900

    refactor: 추후 작업을 위해 비워둠

--- components/MyPage/UserTogetherContents.js
+++ components/MyPage/UserTogetherContents.js
@@ -3,39 +3,21 @@ import { TiLocation } from "react-icons/ti";
 import styles from "./UserTogetherContents.module.css";
 
 function UserTogetherContents() {
-  const [datas, setDatas] = useState([
-    {
-      src: "https://a.cdn-hotels.com/gdcs/production144/d992/418cd5c1-7f91-4c44-9f39-3016b033eaa1.jpg?impolicy=fcrop&w=800&h=533&q=medium",
-      icon_src: "https://cdn-icons-png.flaticon.com/128/2340/2340091.png",
-      alt: "강릉 사진",
-      title: "강릉",
-    },
-    {
-      src: "https://mediahub.seoul.go.kr/wp-content/uploads/2015/01/f9ca3f3426800ac74efd7cd16c38d42f.jpg",
-      icon_src: "https://cdn-icons-png.flaticon.com/128/2340/2340091.png",
-      alt: "서울 사진",
-      title: "서울",
-    },
-    {
-      src: "https://mediahub.seoul.go.kr/wp-content/uploads/2015/01/f9ca3f3426800ac74efd7cd16c38d42f.jpg",
-      icon_src: "https://cdn-icons-png.flaticon.com/128/2340/2340091.png",
-      alt: "서울 사진",
-      title: "서울",
-    },
-  ]);
+  const [datas, setDatas] = useState(null);
   console.log(datas);
   return (
     <div className={styles.togetherContainer}>
       <div className={styles.togetherBox}>
-        {datas.map((data, index) => (
-          <div className={styles.togetherContent} key={index}>
-            <img src={data.src} alt={data.alt} className={styles.togetherPic} />
-            <div className={styles.IconContainer}>
-              <TiLocation className={styles.Icon} />
-              <p className={styles.IconText}>{data.title}</p>
+        {datas &&
+          datas.map((data, index) => (
+            <div className={styles.togetherContent} key={index}>
+              <img src={data.src} alt={data.alt} className={styles.togetherPic} />
+              <div className={styles.IconContainer}>
+                <TiLocation className={styles.Icon} />
+                <p className={styles.IconText}>{data.title}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
