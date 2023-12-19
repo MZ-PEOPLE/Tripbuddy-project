8c7e15f95ddc306f65dceb469c345b71d46f15cc
yeong <gukgu6060@gmail.com>
2023-12-19 23:41:46 +0900

    design: 채팅 입력창, 전송버튼 컬러 통일

--- components/chatroom/InputChat.js
+++ components/chatroom/InputChat.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./InputChat.module.css";
-
 export default function InputChat() {
     return (
         <input className={styles.inputBar}></input>
--- components/chatroom/SendBtn.js
+++ components/chatroom/SendBtn.js
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "./SendBtn.module.css";
 export default function SendBtn() {
     return (
-        <sendchat>
+        <>
             <div className={styles.mainFrame}>
                 <div className={styles.sendBtnFrame}>
                     <button className={styles.sendBtn}>전송</button>
                 </div>
             </div>
-        </sendchat >
+        </>
     );
 
 }
