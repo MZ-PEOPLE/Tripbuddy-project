318cf9efb7cc4eb6236b7e6475520d5b4dcee031
77JY <kwn03111@naver.com>
2023-12-19 23:15:52 +0900

    refactor: 정상응답을 잘못된 응답으로 보내주는 부분 수정

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     const { id } = req.query;
     const writer = await User.findOne({ _id: id }).select("name about gender profileImage age id");
 
-    return res.status(500).json(writer);
+    return res.status(200).json(writer);
   } catch (error) {
     return res.status(500).json({ message: "실패했어용 아마 없는사용자인듯?" });
   }
