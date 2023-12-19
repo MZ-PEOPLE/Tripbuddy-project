d31cad9850fbac412ad577b6efcf89221626aa44
77JY <kwn03111@naver.com>
2023-12-20 00:23:38 +0900

    feat: 해당 유저의 프로필에 해당 유저 조회 페이지 링크 연결

--- components/postDetail/UserPostInfo.js
+++ components/postDetail/UserPostInfo.js
@@ -2,42 +2,46 @@ import styles from "./UserPostInfo.module.css";
 import { FaUserGroup } from "react-icons/fa6";
 import { CiLocationOn } from "react-icons/ci";
 import HeartIconToggle from "./HeartIconToggle";
+import Link from "next/link";
 
 /* 추후 컴포넌트 분리작업 이 필요한 것 같음
 비대하진 않으나 무슨작업을 하는지 잘 모르겠음
 4가지 컴포넌트로 분해후 불러오면 읽기 쉬울듯
 */
-const UserPost = ({ items }) => (
-  <div className={styles.user_post}>
-    <div className={styles.post_info}>
-      <div className={styles.post_box}>
-        <div className={styles.post_iconBox}>
-          <FaUserGroup className={styles.post_iconSize} />
-          <span className={styles.post_info_fontSize}>{items.numberOfPeople}</span>
-        </div>
-        <div className={styles.post_iconBox}>
-          <HeartIconToggle />
-          {/* 하트를 이모티콘이 아닌 버튼으로 교체작업 필요 + 클릭 true일때 빨간하트, false일때 회색
+function UserPost({ items }) {
+  return (
+    <div className={styles.user_post}>
+      <div className={styles.post_info}>
+        <div className={styles.post_box}>
+          <div className={styles.post_iconBox}>
+            <FaUserGroup className={styles.post_iconSize} />
+            <span className={styles.post_info_fontSize}>{items.numberOfPeople}</span>
+          </div>
+          <div className={styles.post_iconBox}>
+            <HeartIconToggle />
+            {/* 하트를 이모티콘이 아닌 버튼으로 교체작업 필요 + 클릭 true일때 빨간하트, false일때 회색
           백엔드 작업이 되었을땐 유저가 좋아요를 눌렀는지 호출 필요 */}
-          <span className={styles.post_info_fontSize}>{items.like}</span>
+            <span className={styles.post_info_fontSize}>{items.like}</span>
+          </div>
         </div>
-      </div>
-      <div className={styles.post_location}>
-        <CiLocationOn className={styles.post_iconSize} />
-        <span className={styles.post_info_fontSize}>{items.location}</span>
-      </div>
-      <div className={styles.post_title}>{items.title}</div>
-      <div className={styles.userInfo}>
-        <div className={styles.userImgBox}>
-          <img src={items.userImg} className={styles.userImg} />
+        <div className={styles.post_location}>
+          <CiLocationOn className={styles.post_iconSize} />
+          <span className={styles.post_info_fontSize}>{items.location}</span>
         </div>
-        <div className={styles.userInfoDetail}>
-          <div className={`${styles.userName} ${styles.userInfoDetailBox}`}>{items.username}</div>
-          <div className={`${styles.timeStemp} ${styles.userInfoDetailBox}`}>{items.timeStemp}</div>
+        <div className={styles.post_title}>{items.title}</div>
+        <div className={styles.userInfo}>
+          <div className={styles.userImgBox}>
+            <Link href={`/user/${items._id}`}>
+              <img src={items.userImg} className={styles.userImg} />
+            </Link>
+          </div>
+          <div className={styles.userInfoDetail}>
+            <div className={`${styles.userName} ${styles.userInfoDetailBox}`}>{items.username}</div>
+            <div className={`${styles.timeStemp} ${styles.userInfoDetailBox}`}>{items.timeStemp}</div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+}
 export default UserPost;
