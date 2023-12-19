b24fd9bbf239a61f8adaf06a8f9a39976b2bcb14
77JY <114325148+77JY@users.noreply.github.com>
2023-12-19 16:40:19 +0900

    feat: Next.js 서버 설정 및 이미지 파일 변경 감시 기능 추가

---
+++ server.js
@@ -0,0 +1,27 @@
+const next = require("next");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = http.createServer((req, res) => {
+    return handle(req, res);
+  });
+
+  const imagesPath = path.join(__dirname, "public", "img");
+  fs.watch(imagesPath, (eventType, filename) => {
+    if (eventType === "rename") {
+      console.log(` ${filename}파일이 CRUD중 뭔가.. 뭔가됨`);
+      // 여기에 추가 작업을 수행합니다.
+    }
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log("3000번 포트 준비됨");
+  });
+});
