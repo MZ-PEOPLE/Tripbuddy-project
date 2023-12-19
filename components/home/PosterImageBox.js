a41cfa7b76d4920ab4f7e0b5388d501140caa4f9
77JY <kwn03111@naver.com>
2023-12-19 09:38:09 +0900

    refactor: props로 전달받은 위치정보의 특정부분만 출력
    props로 받은 후 대분류의 위치기준으로 출력하게 변환한후 출력하게함

--- components/home/PosterImageBox.js
+++ components/home/PosterImageBox.js
@@ -3,13 +3,15 @@ import HeartIconToggle from "../postDetail/HeartIconToggle";
 import { TiLocation } from "react-icons/ti";
 
 const PosterImage = ({ location, isLike, imgSrc }) => {
+  const locations = location.split(" ")[2];
+
   return (
     <div className={styles.imgBox}>
       <img src={imgSrc} className={styles.userImg}></img>
       <div className={styles.infoBox}>
         <div className={styles.locationsBox}>
           <TiLocation className={styles.marker_img} />
-          <div className={styles.location}>{location}</div>
+          <div className={styles.location}>{locations}</div>
         </div>
         <div className={styles.likeBtn}>
           <HeartIconToggle />
