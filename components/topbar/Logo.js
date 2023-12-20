5e30cdb0789f82b4f16b700fb43701ecefda3a2e
77JY <kwn03111@naver.com>
2023-12-20 21:03:12 +0900

    refactor: 이미지 경로 수정

--- components/topbar/Logo.js
+++ components/topbar/Logo.js
@@ -1,7 +1,7 @@
 import styles from "@/components/topbar/Logo.module.css";
 
 const Logo = () => {
-  return <img src="img/logo.png" className={styles.logo} />;
+  return <img src="/img/logo.png" className={styles.logo} />;
 };
 
 export default Logo;
